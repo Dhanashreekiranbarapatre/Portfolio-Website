0a30e5c5321e354677a4ede83b5f4903488568ba
Dhanashreekiranbarapatre <152379685+Dhanashreekiranbarapatre@users.noreply.github.com>
2024-02-06 16:50:18 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Add custom JavaScript here
+
+function runTypingEffect()
+{
+    const text = 'I am Dhanashree Barapatre';
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 300;
+
+    typeText(text,typingElement,typingDelay);
+}
+
+function typeText(text,typingElement,delay)
+{
+    for(let i=0; i<text.length; i++)
+    {
+        setTimeout(()=>
+        {
+            typingElement.textContent +=text.charAt(i);
+        }, delay*i);
+    }
+}
+document.addEventListener('DOMContentLoaded',runTypingEffect);
